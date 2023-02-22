1bcee1f3985e78aa33e070cb77a374277d5a4fda
Constantine Isaev <constantine.isaev@gmail.com>
2023-02-22 20:28:17 +0400

    add solution for 238

--- array/238_product_of_array_except_self.js
+++ array/238_product_of_array_except_self.js
@@ -1,9 +1,34 @@
-// solution WITH DIVISTION
+// solution without division (time n space 1)
 /**
  * @param {number[]} nums
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
+    let resultProduct = [1];
+    let leftProductCurrent = 1;
+    let rightProductCurrent = 1;
+    // fill in right product array in result array
+    for (let j = nums.length - 2; j >= 0; j--) {
+        rightProductCurrent *= nums[j+1]
+        resultProduct.push(rightProductCurrent);
+    }
+    resultProduct.reverse()
+    // update result product array
+    for (let i = 1; i < nums.length; i++) {
+        leftProductCurrent *= nums[i-1];
+        resultProduct[i] = leftProductCurrent * resultProduct[i]
+    }
+    return resultProduct;
+};
+
+console.log(productExceptSelf([-1,1,0,-3,3]))
+
+// solution WITH DIVISTION
+/**
+ * @param {number[]} nums (time n space 1)
+ * @return {number[]}
+ */
+var productExceptSelfWithDivision = function(nums) {
     let zeros = 0;
     let zeroIndex = undefined;
     let commonProduct = 1;
@@ -21,4 +46,5 @@ var productExceptSelf = function(nums) {
     return nums.map(el => commonProduct / el);
 };
 
-console.log(productExceptSelf([-1,1,0,-3,3]))
+console.log(productExceptSelfWithDivision([-1,1,0,-3,3]))
+
